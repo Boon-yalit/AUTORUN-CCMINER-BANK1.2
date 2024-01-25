68d1d0ce4f5963b536990eda3c561615f5451f6f
Boon-yalit <144957308+Boon-yalit@users.noreply.github.com>
2024-01-26 02:23:56 +0700

    Update config.py

--- mobile-mining/config.py
+++ mobile-mining/config.py
@@ -4,11 +4,10 @@ import os, json, requests
 def banner():
     os.system("clear")
     print("\033[1;34;40m")
-    os.system("figlet -f big VERUS")
-    os.system("figlet -f digital github version 1.2")
+    os.system("figlet -f ANSI_Shadow VERUS")
+    os.system("figlet -f digital Github version")
     print("\033[00m\n")
-    print("\033[96mEdit by PICHET SAENGTEWAN\033[0m")
-    print("\033[96mDebug Colors by AMARIN PRAPRIWIT\033[0m")
+    print("\033[96mDeveloper by PICHET SAENGTAWAN\033[0m")
     print("\033[36m\033[0m")
     
     print(f"\n\033[91mกำลัง Run Mining\033[0m\n")
